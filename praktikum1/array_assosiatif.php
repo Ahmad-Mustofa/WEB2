69b784eec3c9a0fb93492399f40ee63e99794e11
Ahmad-Mustofa <ahma23181si@student.nurulfikri.ac.id>
2024-02-21 19:24:15 +0700

    first commit

---
+++ praktikum1/array_assosiatif.php
@@ -0,0 +1,9 @@
+<?php
+$mhs1 = [
+    'nama' => 'Ahmad Mustofa Mubarok', 
+    'nim' => '0112', 
+    'jurusan' => 'Sistem Informasi',
+];
+
+// memanggil array asosiatif
+echo 'nama: ' . $mhs1['nama'];
