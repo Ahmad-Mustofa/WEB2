b08b6e1ed2ee5fd4bbba521a86f12ca7cb41ead3
Reyysiregar <raih23261si@student.nurulfikri.ac.id>
2024-03-20 10:47:39 +0700

    push praktikum 2

--- praktikum3/Form _regist.php
+++ praktikum3/Form _regist.php
@@ -1,20 +1,31 @@
 <?php
 $prodi = [
-    'SI' => 'Sistem Informasi',
-    'TI' => 'Teknik Informatika',
-    'BD' => 'Bisnis Digital'
+    'Si' => 'Sistem Informasi',
+    'Ti' => 'Teknik Informatika',
+    'Bisnis Digital' => 'Bisnis Digital',
 ];
+
+
+
+$domisili = [
+    'jakarta',
+    'Bogor',
+    'Depok',
+    'Tanggerang',
+    'Bekasi',
+];
+
 $skills = [
     'HTML' => 10,
     'CSS' => 10,
     'JavaScript' => 20,
-    'RWD Bootstrap' => 20,
+    'RWD Boostrap' => 20,
     'PHP' => 30,
-    'Phyton' => 30,
-    'Java' => 50
-];
+    'Python' => 30,
+    'Java' => 50,
+    
+    ];
 
-$domisili = ['Jakarta', 'Depok', 'Bogor', 'Tanggerang', 'Bekasi', 'Lainnya'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,59 +33,83 @@ $domisili = ['Jakarta', 'Depok', 'Bogor', 'Tanggerang', 'Bekasi', 'Lainnya'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Registrasi</title>
+    <title>Form Registrasi - IT Club</title>
+    <style>
+        fieldset{
+            background: #12372A;
+            border-radius: 15px;
+        }
+        .top{
+            color: #ADBC9F;
+            font-family: roboto, sans-serif;
+            font-weight: 600;
+            
+        }
+
+        .hero{
+        color: white;
+        font-family: poppins, sans-serif;
+        }
+        
+    </style>
 </head>
 
 <body>
     <fieldset class="mx-5 border p-2">
-        <legend>Form Registrasi IT Club</legend>
+        <legend class="top">Form Registrasi - IT Club</legend>
+
+        <!--main-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <form action="proses_registrasi.php" method="POST">
+        <form class="hero" action="proses_registrasi.php" method="POST">
             <div class="form-group row">
                 <label for="nim" class="col-4 col-form-label">NIM</label>
                 <div class="col-8">
-                    <input id="nim" name="nim" type="text" class="form-control" required="required">
+                    <input id="nim" name="nim" type="text" class="form-control" required="required" placeholder="Masukkan NIM">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="nama" class="col-4 col-form-label">Nama Lengkap</label>
                 <div class="col-8">
-                    <input id="nama" name="nama" type="text" class="form-control" required="required">
+                    <input id="nama" name="nama" type="text" class="form-control" required="required" placeholder="Nama Lengkap">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-4">Jenis Kelamin</label>
                 <div class="col-8">
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="Laki-Laki" required="required">
+                        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="laki-laki"
+                            required="required">
                         <label for="jk_0" class="custom-control-label">Laki-Laki</label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="Perempuan" required="required">
+                        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="perempuan"
+                            required="required">
                         <label for="jk_1" class="custom-control-label">Perempuan</label>
                     </div>
                 </div>
             </div>
             <div class="form-group row">
-                <label for="prodi" class="col-4 col-form-label">Program Studi</label>
+                <label for="prodi" class="col-4 col-form-label">Prodi</label>
                 <div class="col-8">
-                    <select id="prodi" name="prodi" class="custom-select" required="required">
-                        <?php foreach ($prodi as $key => $value) : ?>
+                    <select id="prodi" name="prodi" required="required" class="custom-select">
+                        <?php foreach($prodi as $key => $value): ?>
                             <option value="<?php echo $key ?>"><?php echo $value ?></option>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
                     </select>
                 </div>
             </div>
             <div class="form-group row">
-                <label class="col-4">Skill Web &amp; Programming</label>
+                <label class="col-4">skill web &amp; programing</label>
                 <div class="col-8">
-                    <?php foreach ($skills as $key => $value) : ?>
-                        <div class="custom-control custom-checkbox custom-control-inline">
-                            <input name="skill[]" id="skill<?php echo $key ?>" type="checkbox" class="custom-control-input" value="<?php echo $key ?>">
-                            <label for="skill<?php echo $key ?>" class="custom-control-label"><?php echo $key ?></label>
-                        </div>
+                <?php foreach($skills as $key => $value): ?>
+                    <div class="custom-control custom-checkbox custom-control-inline">
+                    
+                        <input name="skill[]" id="skill<?php echo $key ?>" type="checkbox" class="custom-control-input" value="<?php echo $key ?>">
+                        <label for="skill<?php echo $key ?>" class="custom-control-label"><?php echo $key ?></label>
+                        
+                    </div>
                     <?php endforeach; ?>
                 </div>
             </div>
@@ -82,16 +117,16 @@ $domisili = ['Jakarta', 'Depok', 'Bogor', 'Tanggerang', 'Bekasi', 'Lainnya'];
                 <label for="domisili" class="col-4 col-form-label">Domisili</label>
                 <div class="col-8">
                     <select id="domisili" name="domisili" class="custom-select" required="required">
-                        <?php foreach ($domisili as $value) : ?>
-                            <option value="<?php echo $value ?>"><?php echo $value ?></option>
-                        <?php endforeach; ?>
+                        <?php foreach($domisili as $value): ?>
+                            <option value="<?php echo $value?>"> <?php echo $value ?></option>
+                            <?php endforeach; ?>
                     </select>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="email" class="col-4 col-form-label">Email</label>
                 <div class="col-8">
-                    <input id="email" name="email" type="text" class="form-control" required="required">
+                    <input id="email" name="email" type="text" class="form-control" required="required" placeholder="Masukkan Email">
                 </div>
             </div>
             <div class="form-group row">
@@ -101,6 +136,7 @@ $domisili = ['Jakarta', 'Depok', 'Bogor', 'Tanggerang', 'Bekasi', 'Lainnya'];
             </div>
         </form>
     </fieldset>
+
 </body>
 
-</html>
+</<?php echo $key ?>>
