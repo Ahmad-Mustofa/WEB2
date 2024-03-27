04e08b6fe5e7f2a471e7b9fdbf088e2389ef2078
Ahmad-Mustofa <ahma23181si@student.nurulfikri.ac.id>
2024-03-27 11:18:36 +0700

    praktikum dan tugas 5

---
+++ praktikum6/index.php
@@ -0,0 +1,38 @@
+<?php
+include_once('top.php');
+include_once('menu.php');
+?>
+
+<head>
+
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="Template/css/styles.css" rel="stylesheet" />
+
+</head>
+<style>
+    h1 {
+        font-family: poppins, sans-serif;
+        font-weight: 500;
+        text-decoration: underline;
+    }
+</style>
+
+<body>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard-Admin</h1>
+        </div>
+    </main>
+    <!--charts-->
+
+
+
+
+
+
+
+
+
+<?php
+include_once('bottom.php');
+?>
